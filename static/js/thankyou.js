529bd981e4f22d0109d25936483306716b1ca370
Andy Wang <zxcvbn884488@gmail.com>
2021-05-31 18:32:09 +0800

    add /thankyou loading animation

--- static/js/thankyou.js
+++ static/js/thankyou.js
@@ -53,6 +53,9 @@ let thankyouViews = {
       const orderData = thankyouModels.orderData;
       const welcomeElement = document.getElementsByClassName('welcome')[0];
       const orderInformationElement = document.getElementsByClassName('order-information')[0];
+      const spinner = document.getElementsByClassName('spinner')[0];
+
+      orderInformationElement.removeChild(spinner);
 
       if (orderData == null) {
          welcomeElement.innerText = `${username}，您好：`;
@@ -63,7 +66,6 @@ let thankyouViews = {
          const noResultContent = document.createTextNode('搜尋不到該訂單');
          noResultElement.appendChild(noResultContent);
          orderInformationElement.appendChild(noResultContent);
-   
       } else if (orderData.status === 1) {
          welcomeElement.innerText = `${username}，您好：`;
       
@@ -88,6 +90,9 @@ let thankyouViews = {
       }
    },
    createAPIElement: function(orderData) {
+      const orderInformationElement = document.getElementsByClassName('order-information')[0];
+      for (let i = 0; i < orderInformationElement.children.length; i++) orderInformationElement.children[i].classList.remove('hidden');
+
       const welcomeElement = document.getElementsByClassName('welcome')[0];
       welcomeElement.innerText = `${username}，感謝您的購買！您的訂單資訊如下：`;
    
@@ -124,42 +129,81 @@ let thankyouViews = {
          actualTime = '下午 2 點到晚上 9 點';
       }
    
-      let orderNumberContent = document.createTextNode(`訂單編號：${orderNumber}`);
+      const orderNumberContent = document.createTextNode(`訂單編號：${orderNumber}`);
       orderNumberElement.appendChild(orderNumberContent);
    
-      let imageElement = document.createElement('img');
+      const imageElement = document.createElement('img');
       imageElement.src = image;
+      const loadingElement = document.createElement('div');
+      loadingElement.innerText = 'Loading...';
+      loadingElement.classList.add('loading');
+      attractionImageElement.appendChild(loadingElement);
       attractionImageElement.appendChild(imageElement);
+
+      imageElement.onload = function() {
+         attractionImageElement.removeChild(loadingElement);
+      };
    
-      let nameContent = document.createTextNode(`台北一日遊：${attractionName}`);
+      const nameContent = document.createTextNode(`台北一日遊：${attractionName}`);
       attractionNameElement.appendChild(nameContent);
    
-      let dateContent = document.createTextNode(date);
+      const dateContent = document.createTextNode(date);
       dateElement.appendChild(dateContent);
    
-      let timeContent = document.createTextNode(actualTime);
+      const timeContent = document.createTextNode(actualTime);
       timeElement.appendChild(timeContent);
    
       let feeContent = document.createTextNode(`新台幣 ${price} 元`);
       feeElement.appendChild(feeContent);
    
-      let addressContent = document.createTextNode(address);
+      const addressContent = document.createTextNode(address);
       addressElement.appendChild(addressContent);
    
-      let contactNameContent = document.createTextNode(contactName);
+      const contactNameContent = document.createTextNode(contactName);
       contactNameElement.appendChild(contactNameContent);
    
-      let contactEmailContent = document.createTextNode(contactEmail);
+      const contactEmailContent = document.createTextNode(contactEmail);
       contactEmailElement.appendChild(contactEmailContent);
    
-      let contactPhoneContent = document.createTextNode(contactPhone);
+      const contactPhoneContent = document.createTextNode(contactPhone);
       contactPhoneElement.appendChild(contactPhoneContent);
+   },
+   createLoadingElement: function() {
+      const orderInformationElement = document.getElementsByClassName('order-information')[0];
+      for (let i = 0; i < orderInformationElement.children.length; i++) orderInformationElement.children[i].classList.add('hidden');
+
+      const spinner = document.createElement('div');
+      spinner.classList.add('spinner');
+
+      const spinnerText = document.createElement('div');
+      spinnerText.classList.add('spinner-text');
+      spinnerText.innerText = 'Loading';
+
+      const spinnerSectorRed = document.createElement('div');
+      spinnerSectorRed.classList.add('spinner-sector');
+      spinnerSectorRed.classList.add('spinner-sector-red');
+
+      const spinnerSectorBlue = document.createElement('div');
+      spinnerSectorBlue.classList.add('spinner-sector');
+      spinnerSectorBlue.classList.add('spinner-sector-blue');
+
+      const spinnerSectorGreen = document.createElement('div');
+      spinnerSectorGreen.classList.add('spinner-sector');
+      spinnerSectorGreen.classList.add('spinner-sector-green');
+
+      spinner.appendChild(spinnerText);
+      spinner.appendChild(spinnerSectorRed);
+      spinner.appendChild(spinnerSectorBlue);
+      spinner.appendChild(spinnerSectorGreen);
+      orderInformationElement.appendChild(spinner);
    }
 };
 
 // thankyouControllers
 let thankyouControllers = {
    init: async function() {
+      thankyouViews.createLoadingElement();
+      
       await thankyouModels.fetchGetUserAPI()
          .then(() => thankyouModels.userData = null);
 
